dabd282a540df238553c6fc8e85b970626c6ca31
ma7moudk3 <mkk28112000@gmail.com>
2023-08-01 12:33:41 +0300

    زز

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+            <p>
+                {{ error }}
+            </p>
+        </base-dialog>
+        <base-dialog fixed :show="isLoading" title="Loading.......">
+            <base-spinner></base-spinner>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitForm">
+                <div class="form-control">
+                    <label for="email">Your E-Mail</label>
+                    <input type="email" id="email" v-model="email" />
+                </div>
+                <div class="form-control">
+                    <label for="password">Your Password</label>
+                    <input type="password" id="password" v-model="password" />
+                </div>
+                <p v-if="!formIsValid">Please enter a valid email and password!</p>
+                <base-button>{{ submitButtonCaption }}</base-button>
+                <base-button mode="flat" type="button" @click="switchAuthMode('signup')">{{ switchButtonCaption
+                }}</base-button>
+            </form>
+        </base-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            formIsValid: true,
+            mode: 'login',
+            isLoading: false,
+            error: null,
+        };
+    },
+    methods: {
+        handleError() {
+            this.error = null;
+        },
+        switchAuthMode() {
+            this.mode = this.mode === 'login' ? 'signup' : 'login';
+        },
+        validateForm() {
+            this.formIsValid = true;
+            if (this.email === '' || this.password === '') {
+                this.formIsValid = false;
+            }
+        },
+        async submitForm() {
+            this.validateForm();
+            if (!this.formIsValid) {
+                return;
+            }
+            this.isLoading = true;
+            try {
+                if (this.mode === 'login') {
+                    // Send a request to the login endpoint
+                } else {
+                    await this.$store.dispatch('auth/signup', {
+                        email: this.email,
+                        password: this.password,
+                    });
+                }
+
+            } catch (error) {
+                this.error = error.message || 'Failed to authenticate. Check your login data.';
+            }
+            this.isLoading = false;
+        },
+
+    },
+    computed: {
+        submitButtonCaption() {
+            return this.mode === 'login' ? 'Login' : 'Signup';
+        },
+        switchButtonCaption() {
+            return this.mode === 'login' ? 'Signup instead' : 'Login instead';
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+    margin: 1rem;
+    border-radius: 12px;
+    padding: 1rem;
+}
+
+.form-control {
+    margin: 0.5rem 0;
+}
+
+label {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    display: block;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+    border-color: #3d008d;
+    background-color: #faf6ff;
+    outline: none;
+}
+
+p {
+    color: red;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 import { i18n } from '../src/main.js';
 
@@ -28,6 +29,7 @@ const router = createRouter({
         }, // /coaches/c1/contact
       ],
     },
+    {path: '/auth', component: UserAuth},
     { path: '/register', component: CoachRegistration },
     { path: '/requests', component: RequestsReceived },
     { path: '/:notFound(.*)', component: NotFound },
